0265988049f9fddcbae252ec16657f2c69e3f940
Luiza Petrinina <petrinina95@gmail.com>
2024-11-11 19:25:21 +0200

    Added HTML and CSS in a header component

--- Angular/Workshop - Components/workshop/src/app/main/main.component.ts
+++ Angular/Workshop - Components/workshop/src/app/main/main.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { ThemesListComponent } from "../themes-list/themes-list.component";
+import { PostsListComponent } from "../posts-list/posts-list.component";
 
 @Component({
-  selector: 'app-main',
-  standalone: true,
-  imports: [],
-  templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+    selector: 'app-main',
+    standalone: true,
+    imports: [ThemesListComponent, PostsListComponent],
+    templateUrl: './main.component.html',
+    styleUrl: './main.component.css'
 })
 export class MainComponent {
 
